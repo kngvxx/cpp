7df5f1fecb5feac33cb03248f6ded037012d1441
kngvxx <kiki.puk@op.pl>
2018-04-25 18:06:11 +0200

    Funkcje 2

--- 25.04/main.cpp
+++ 25.04/main.cpp
@@ -2,14 +2,26 @@
 
 using namespace std;
 
-void hello_world()
-{
-cout << "Hello world!" << endl;
-}
+void hello_world();
+
+int dodaj(int l1, int l2);
 
 int main()
 {
     hello_world();
+    cout<<dodaj(5,13)<<endl;
 
     return 0;
+}
+
+void hello_world()
+{
+    cout << "Hello world!" << endl;
+}
+
+int dodaj(int l1, int l2)
+{
+    int suma=l1+l2;
+
+    return suma;
 }
