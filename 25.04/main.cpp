bc5e589d11661a9eb9b59f4c542c826f41778767
kngvxx <kiki.puk@op.pl>
2018-04-25 18:11:27 +0200

    Przeładowanie funkcji

--- 25.04/main.cpp
+++ 25.04/main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 void hello_world();
+string hello_world(int a); //Prze³adowanie funkcji
 
 int dodaj(int l1, int l2);
 
 int main()
 {
     hello_world();
+    cout<<hello_world(1)<<endl;
     cout<<dodaj(5,13)<<endl;
 
     return 0;
@@ -17,6 +19,11 @@ int main()
 void hello_world()
 {
     cout << "Hello world!" << endl;
+}
+
+string hello_world(int a)
+{
+    return "Hello World";
 }
 
 int dodaj(int l1, int l2)
